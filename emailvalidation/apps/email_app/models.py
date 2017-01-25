4dc9f8819c86b2b38dbc2d286fc9724654c46a79
Udaya Tummala <udayatummala1@gmail.com>
2017-01-25 13:18:28 -0500

    added email validation

--- emailvalidation/apps/email_app/models.py
+++ emailvalidation/apps/email_app/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+import re
+
 # Create your models here.
+
+email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class emailManager(models.Manager):
+    def emailValidator(self,email):
+        if email_regex.match(email):
+            Email.objects.create(emailAddress = email)
+            return(True,"Email was a valid ")
+        else:
+            return(False,"Email Address You enetered was Invalid")
+
+
+
+class Email(models.Model):
+    emailAddress = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = emailManager()
--- emailvalidation/apps/email_app/views.py
+++ emailvalidation/apps/email_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Email
 
 # Create your views here.
+
+def index(request):
+    return render(request,"email_app/index.html")
+def process(request):
+    results = Email.objects.emailValidator(request.POST['email'])
+    print results
+    if results[0] == True:
+        context = {
+        'succ':request.POST['email'],
+        'emails':Email.objects.all()
+        }
+        return render(request,'email_app/success.html',context)
+    if results[0] == False:
+        print results[1]
+        context ={
+        'err' : results[1],
+        }
+        return render(request,"email_app/index.html",context)
